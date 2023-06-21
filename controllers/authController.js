33b085d67e87dd694677577e0cea47928da3fd8e
unfoldedRose <60324608+unfoldedRoses@users.noreply.github.com>
2023-06-21 09:42:54 -0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -86,7 +86,7 @@ const login = async (req, res) => {
     //   maxAge: 24 * 60 * 60 * 1000,
     // });
 
-    res.setHeader('Set-Cookie', 'token=token; HttpOnly; Secure; SameSite=None; Max-Age=3600');
+    res.setHeader('Set-Cookie', 'token=token; Secure; SameSite=None; Max-Age=3600');
    
 
 
